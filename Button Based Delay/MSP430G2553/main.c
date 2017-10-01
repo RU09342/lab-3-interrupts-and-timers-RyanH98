09faeacbbdc4150f1f1978b4c3ced0fd2d33fa08
RyanH98 <harer6@students.rowan.edu>
2017-10-01 14:18:14 -0400

    Finalized button delay code for G2553

--- Button Based Delay/MSP430G2553/main.c
+++ Button Based Delay/MSP430G2553/main.c
@@ -1,8 +1,7 @@
 #include <msp430.h>
 
-unsigned int count, temp, time, button, lastButton, buttonOut, lastDebounce, debounce = 0;   //Define the integers, which are used later.
+unsigned int count, time, button, lastButton, timeStart, timeEnd, overflows = 0;   //Define the integers, which are used later.
 unsigned int div = 4;
-#define temp (P1IN & BIT3)
 #define BUTTON BIT3                 //Define "BUTTON" as bit 3
 
 void main(void)
@@ -28,18 +27,6 @@ void main(void)
 
     __bis_SR_register(LPM0 + GIE);  //Enter low power mode with interrupts.
 
-    while(1){
-        if (button != lastButton)
-            lastDebounce = count;
-        if ((count - lastDebounce) > 50)
-            buttonOut = button;
-        if (buttonOut == 1)
-            temp = temp + 1;
-        else
-            div = 65536/temp;
-        lastButton = button;
-    }
-
 }
 
 //Interrupt vector service routine for Timer A0.
@@ -49,11 +36,28 @@ __interrupt void Timer_A (void) {
     count++;
     if (time == 0)
         P1OUT ^= 0x41;                  //Toggle the LED whenever time % div == 0.
+    if (TAIV == 0x0E)
+        overflows++;                    //If the capture/compare register says that the interrupt was caused by an overflow, increment 'overflows'.
 }
 
 #pragma vector=PORT1_VECTOR             //Set the port 1 interrupt routine
 __interrupt void Port_1(void) {
     button ^= 1;                        //Toggle the button variable.
+    P1IE &= ~BUTTON;                    //Turn off the button interrupt enable.
+    __delay_cycles(1);                  //Delay for one clock cycle to debounce the button.
+    P1IE |= BUTTON;                     //Turn on the button interrupt enable.
+
+    if ((button == 1) && (lastButton == 0)){    //If the button has been pressed, this if statement will trigger.
+        timeStart = count;                      //Store the current count in 'timeStart'.
+        overflows = 0;                          //Reset 'overflows'.
+    }
+    else if ((button == 0) && (lastButton == 1)){               //If the button has been depressed, this if statement will trigger.
+        timeEnd = count;                                        //Store the current count in 'timeEnd'.
+        div = ((timeEnd + (overflows * 65536)) - timeStart);    //Update div to be equal to the difference in 'timeEnd' and 'timeStart'...
+                                                                //...taking into account the amount of overflows that occurred between the two.
+    }
+    lastButton = button;                //Update 'lastButton' with 'button'.
+
     P1IFG &= ~BUTTON;                   //P1.3 IFG cleared
     P1IES ^= BUTTON;                    //Toggle the interrupt edge so that this interrupt triggers on the button press and release.
 }
